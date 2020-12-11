1cab0657204656a26662c4ace0750d342eae8881
Joy <joycok@gmail.com>
2020-12-11 10:43:56 +0530

    save

--- DataBase/Export2Pdf.cs
+++ DataBase/Export2Pdf.cs
@@ -570,12 +570,19 @@ namespace DataBase
             int destWidth = (int)(sourceWidth * nPercent);
             int destHeight = (int)(sourceHeight * nPercent);
 
+            //destWidth = size.Width;
+            //destHeight = size.Height;
+
             Bitmap b = new Bitmap(destWidth, destHeight);
             Graphics g = Graphics.FromImage((Image)b);
+
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
             g.InterpolationMode = InterpolationMode.HighQualityBicubic;
 
             g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
             
+
 
             g.Dispose();
 
@@ -669,4 +676,73 @@ namespace DataBase
 
 
     }
+
+    public class ImageProcessor
+    {
+        public Image Resize(Image image, int newWidth, int newHeight, string message)
+        {
+            try
+            {
+                Image newImage = new Bitmap(newWidth, Calculations(image.Width, image.Height, newWidth));
+
+                using (Graphics gr = Graphics.FromImage(newImage))
+                {
+                    gr.SmoothingMode = SmoothingMode.AntiAlias;
+                    gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    gr.DrawImage(image, new Rectangle(0, 0, newImage.Width, newImage.Height));
+
+                    var myBrush = new SolidBrush(Color.FromArgb(70, 205, 205, 205));
+
+                    double diagonal = Math.Sqrt(newImage.Width * newImage.Width + newImage.Height * newImage.Height);
+
+                    Rectangle containerBox = new Rectangle();
+
+                    containerBox.X = (int)(diagonal / 10);
+                    float messageLength = (float)(diagonal / message.Length * 1);
+                    containerBox.Y = -(int)(messageLength / 1.6);
+
+                    Font stringFont = new Font("verdana", messageLength);
+
+                    StringFormat sf = new StringFormat();
+
+                    float slope = (float)(Math.Atan2(newImage.Height, newImage.Width) * 180 / Math.PI);
+
+                    gr.RotateTransform(slope);
+                    gr.DrawString(message, stringFont, myBrush, containerBox, sf);
+                    return newImage;
+                }
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+
+        public int Calculations(decimal w1, decimal h1, int newWidth)
+        {
+            decimal height = 0;
+            decimal ratio = 0;
+
+
+            if (newWidth < w1)
+            {
+                ratio = w1 / newWidth;
+                height = h1 / ratio;
+
+                return (int)height;
+            }
+
+            if (w1 < newWidth)
+            {
+                ratio = newWidth / w1;
+                height = h1 * ratio;
+                return (int)height;
+            }
+
+            return (int) height;
+        }
+
+    }
+
 }
